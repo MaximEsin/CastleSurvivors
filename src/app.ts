8e8a95a966ed87c62a7b6b5f8c87487ef52e54c1
Max <maximesin21@gmail.com>
2024-01-28 15:30:51 +0100

    feat: 🎸 Add a resizeable background
    A new class is added to handle backgrounds. It resizes together with a
    game. Also background image is placed in public folder

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
+// src/main.ts
 import * as PIXI from 'pixi.js';
+import { Background } from './classes/Background';
 
 // Create PIXI Application
 const app = new PIXI.Application({
   width: window.innerWidth,
   height: window.innerHeight,
-  backgroundColor: 0x1099bb,
 });
 
 // Append PIXI Application to the document body
 document.body.appendChild(app.view as unknown as Node);
+
+// Create a background instance
+const background = new Background('./public/Backgrounds/CastleBG.jpg', app);
 
 // Resize PIXI Application when the window is resized
 window.addEventListener('resize', () => {
---
+++ src/classes/Background.ts
@@ -0,0 +1,21 @@
+import * as PIXI from 'pixi.js';
+
+export class Background {
+  private sprite: PIXI.Sprite;
+
+  constructor(imagePath: string, app: PIXI.Application) {
+    this.sprite = PIXI.Sprite.from(imagePath);
+    this.sprite.width = app.screen.width;
+    this.sprite.height = app.screen.height;
+    app.stage.addChild(this.sprite);
+
+    this.addResizeListener(app);
+  }
+  // Resize the background when the window is resized
+  private addResizeListener(app: PIXI.Application): void {
+    window.addEventListener('resize', () => {
+      this.sprite.width = app.screen.width;
+      this.sprite.height = app.screen.height;
+    });
+  }
+}
