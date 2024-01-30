2856170d0bcc82034b66a4110779a32d4eaa2a4e
Max <maximesin21@gmail.com>
2024-01-30 15:05:01 +0100

    refactor: 💡 Move setRandomPosition method to enemy class
    setrandomPosition method will be shared by all enemy types, so it was
    moved to enemy class

--- src/classes/Enemies/Enemy.ts
+++ src/classes/Enemies/Enemy.ts
@@ -16,5 +16,32 @@ export class Enemy {
     throw new Error('createEnemySprite must be implemented in derived classes');
   }
 
+  protected setRandomPosition(animation: PIXI.AnimatedSprite): void {
+    const screenWidth = this.app.screen.width;
+    const screenHeight = this.app.screen.height;
+
+    const borderOffset = -150;
+
+    // Randomly choose whether to spawn near the horizontal or vertical border
+    const spawnNearHorizontal = Math.random() < 0.5;
+
+    if (spawnNearHorizontal) {
+      // Spawn near the left or right border
+      const spawnOnLeft = Math.random() < 0.5;
+      animation.x = spawnOnLeft
+        ? Math.max(0, -borderOffset) // Ensure enemy stays inside left border
+        : Math.min(screenWidth, screenWidth + borderOffset); // Ensure enemy stays inside right border
+
+      animation.y = Math.random() * screenHeight;
+    } else {
+      // Spawn near the top or bottom border
+      const spawnOnTop = Math.random() < 0.5;
+      animation.x = Math.random() * screenWidth;
+      animation.y = spawnOnTop
+        ? Math.max(0, -borderOffset) // Ensure enemy stays inside top border
+        : Math.min(screenHeight, screenHeight + borderOffset); // Ensure enemy stays inside bottom border
+    }
+  }
+
   public update(): void {}
 }
--- src/classes/Enemies/Mushroom.ts
+++ src/classes/Enemies/Mushroom.ts
@@ -25,32 +25,5 @@ export class Mushroom extends Enemy {
     return animation;
   }
 
-  private setRandomPosition(animation: PIXI.AnimatedSprite): void {
-    const screenWidth = this.app.screen.width;
-    const screenHeight = this.app.screen.height;
-
-    const borderOffset = -150;
-
-    // Randomly choose whether to spawn near the horizontal or vertical border
-    const spawnNearHorizontal = Math.random() < 0.5;
-
-    if (spawnNearHorizontal) {
-      // Spawn near the left or right border
-      const spawnOnLeft = Math.random() < 0.5;
-      animation.x = spawnOnLeft
-        ? Math.max(0, -borderOffset) // Ensure Mushroom stays inside left border
-        : Math.min(screenWidth, screenWidth + borderOffset); // Ensure Mushroom stays inside right border
-
-      animation.y = Math.random() * screenHeight;
-    } else {
-      // Spawn near the top or bottom border
-      const spawnOnTop = Math.random() < 0.5;
-      animation.x = Math.random() * screenWidth;
-      animation.y = spawnOnTop
-        ? Math.max(0, -borderOffset) // Ensure Mushroom stays inside top border
-        : Math.min(screenHeight, screenHeight + borderOffset); // Ensure Mushroom stays inside bottom border
-    }
-  }
-
   public update(): void {}
 }
